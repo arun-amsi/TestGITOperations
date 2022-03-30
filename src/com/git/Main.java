1ee4bbf136b8058fa252dacc5b0b56d05ef1f8f0
Arun Ammasai <arun.ammasai@outlook.com>
2022-03-30 17:27:52 +0530

    calculated method

--- src/com/git/Main.java
+++ src/com/git/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World ....");
+        calculate(10,20);
+    }
+
+    static void calculate(int a, int b){
+        System.out.println("Sum is :"+(a+b));
     }
 }
